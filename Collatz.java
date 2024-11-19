9e54140d83a0bbff9ebf6c974cde05eb51e1d78c
Shahaf S <shahafs@Shahafs-MacBook-Pro.local>
2024-11-19 15:43:20 +0200

    forgot to put en else operator

--- Collatz.java
+++ Collatz.java
@@ -40,8 +40,10 @@ public class Collatz {
 		}
 		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
 		}
-
-		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		else
+		{
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
 
 	}
 }
